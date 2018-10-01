8d765a7feda582d2640733320937ab87487dc118
Mallory Feuer <mallory.feuer@gmail.com>
2018-10-01 18:20:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,5 +15,6 @@ function addNewElementAsLi() {
 }
 
 function addNewLiOnClick() {
-  let submit = document.querySelector('input[type="submit"]')
+  let submit = document.querySelector('input[type="submit"]');
+  
 }
