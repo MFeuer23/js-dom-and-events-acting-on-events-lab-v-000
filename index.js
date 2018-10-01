43254142d1db666fce3ab5946225cc7dcc1f7b2c
Mallory Feuer <mallory.feuer@gmail.com>
2018-10-01 19:06:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,5 +20,8 @@ function addNewElementAsLi() {
 function addNewLiOnClick() {
   let submit = document.querySelector('input[type="submit"]');
   submit.addEventListener('click', addNewElementAsLi);
+}
+
+function clearEmployeeListOnLinkClick() {
   
 }
